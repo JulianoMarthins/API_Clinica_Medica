de7aec24ddf853942749087b0ca7663b3ac77dc4
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-01-03 13:46:04 -0300

    Primeira etapa da API Concluída.

--- pacientes/models.py
+++ pacientes/models.py
@@ -6,7 +6,7 @@ class Pacientes(models.Model):
 
     id_paciente = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100, blank=True, null=True)
-    nascimento = models.DateTimeField(blank=True, null=True)
+    nascimento = models.DateField(blank=True, null=True)
     endereco = models.CharField(max_length=80, blank=True, null=True)
     num_endereco = models.IntegerField(blank=True, null=True)
     bairro = models.CharField(max_length=60, blank=True, null=True)
@@ -23,27 +23,28 @@ class Pacientes(models.Model):
 
 
     """
-            Observações para fins didáticos.
+        Observações para fins didáticos.
     
-            O 'id_paciente' utilizada um método de models para auto incrementar valores para a 
-        id do paciente, em seu argumento, tornamos esta id como chave primária para o banco de dados
+        O 'id_paciente' utilizada um método de models para auto incrementar valores para a 
+    id do paciente, em seu argumento, tornamos esta id como chave primária para o banco de
+    dados
     
     
-            Durante a criação dos atributos da classe, utilizamos nos argumentos o valor TRUE para 
-        os parâmetros blank e null, estes significam que o atributo pode ser deixado em branco ou 
-        nulo durante o preenchimento do cadastro do cliente. 
+        Durante a criação dos atributos da classe, utilizamos nos argumentos o valor TRUE para 
+    os parâmetros blank e null, estes significam que o atributo pode ser deixado em branco ou 
+    nulo durante o preenchimento do cadastro do cliente. 
     
-            A data de cadastro utilizada a função DateField, onde seu parâmetro auto_now faz com 
-        que a data do dia em que o cliente seja cadastrado, seja armazenado no banco de dados.
+        A data de cadastro utilizada a função DateField, onde seu parâmetro auto_now faz com 
+    que a data do dia em que o cliente seja cadastrado, seja armazenado no banco de dados.
         
         
-            Dentro do arquivo models, passamos os atributos da classe, neste caso, da classe cliente, 
-        passamos todos os dados que queremos receber e armazenar no banco de dados, para poder
-        cadastrar um cliente em nosso sistema.
-            Campos como id, nome, endereço, rg, cpf, aniversário, são exemplo de atributos que a 
-        classe cliente pode receber e que serão uteis a nosso sistema, logo, podemos classificar o 
-        arquivo models como os atributos do objeto que serão salvos no banco de dados do nosso 
-        sistema. 
+        Dentro do arquivo models, passamos os atributos da classe, neste caso, da classe cliente, 
+    passamos todos os dados que queremos receber e armazenar no banco de dados, para poder
+    cadastrar um cliente em nosso sistema.
+        Campos como id, nome, endereço, rg, cpf, aniversário, são exemplo de atributos que a 
+    classe cliente pode receber e que serão uteis a nosso sistema, logo, podemos classificar o 
+    arquivo models como os atributos do objeto que serão salvos no banco de dados do nosso 
+    sistema. 
         
     
     """
